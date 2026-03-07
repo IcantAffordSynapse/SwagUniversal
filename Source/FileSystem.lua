42dbfce1a9bc488583b4c5e1c3bfb2c67bb76b1e
esore <88977097+IcantAffordSynapse@users.noreply.github.com>
2026-03-08 00:05:03 +1100

    Update FileSystem.lua

--- Source/FileSystem.lua
+++ Source/FileSystem.lua
@@ -1,1 +1,76 @@
+-- SWAG Universal File System
+-- 07/03/2026
 
+if not getgenv().FileSystemFuncs then 
+    return "File System Unsupported." 
+end
+
+local HttpService = game:GetService("HttpService")
+
+local function deepVerify(template, target)
+    for k,v in pairs(template) do
+        if target[k] == nil then
+            if type(v) == "table" then
+                target[k] = {}
+                deepVerify(v, target[k])
+            else
+                target[k] = v
+            end
+        elseif type(v) == "table" and type(target[k]) == "table" then
+            deepVerify(v, target[k])
+        end
+    end
+end
+
+local function serialize(value, indent)
+    indent = indent or 0
+    local spacing = string.rep("    ", indent)
+
+    if type(value) == "table" then
+        local str = "{\n"
+        for k,v in pairs(value) do
+            local key
+
+            if type(k) == "string" then
+                key = k
+            else
+                key = "["..tostring(k).."]"
+            end
+
+            str = str .. spacing.."    "..key.." = "..serialize(v, indent + 1)..",\n"
+        end
+        return str .. spacing.."}"
+    elseif type(value) == "string" then
+        return string.format("%q", value)
+    else
+        return tostring(value)
+    end
+end
+
+local files = {
+    ["SavedData.lua"] = loadstring(game:HttpGet("https://raw.githubusercontent.com/IcantAffordSynapse/SwagUniversal/refs/heads/main/Templates/SavedData.lua"))(),
+    ["Settings.lua"] = loadstring(game:HttpGet("https://raw.githubusercontent.com/IcantAffordSynapse/SwagUniversal/refs/heads/main/Templates/Settings.lua"))()
+}
+
+if not isFolder("SWAG__UNIVERSAL") then
+    makeFolder("SWAG__UNIVERSAL")
+end
+
+for name, templateTable in pairs(files) do
+    local path = "SWAG__UNIVERSAL/" .. name
+
+    if not isFile(path) then
+        writeFile(path, "return "..serialize(templateTable))
+    else
+        local success, userTable = pcall(loadstring(readFile(path)))
+
+        if success and type(userTable) == "table" then
+            deepVerify(templateTable, userTable)
+            writeFile(path, "return "..serialize(userTable))
+        else
+            writeFile(path, "return "..serialize(templateTable))
+        end
+    end
+end
+
+return true
