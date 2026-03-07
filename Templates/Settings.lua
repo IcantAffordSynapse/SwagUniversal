6d42daaff05bc09b3509d948f5bbb6586b94cc78
esore <88977097+IcantAffordSynapse@users.noreply.github.com>
2026-03-07 23:48:19 +1100

    Create Settings.lua

---
+++ Templates/Settings.lua
@@ -0,0 +1,14 @@
+local settings = {
+    ToggleSettings = {
+        ToggleKey = Enum.KeyCode.L
+    },
+    ThemeSettings = {
+        PrimaryColor = Color3.fromRGB(255, 80, 80)
+    },
+    PrivacySettings = {
+        RedactUsername = false,
+        RedactDisplay = false,
+        RedactUserID = false,
+        RedactPfp = false
+    }
+}
